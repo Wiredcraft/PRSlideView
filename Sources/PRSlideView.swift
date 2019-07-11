e68136e51516bb8aa1c43ea4aed2277209b03225
Elethom Hunter <elethomhunter@gmail.com>
2019-07-11 11:18:49 +0800

    Create Xcode project

---
+++ Sources/PRSlideView.swift
@@ -0,0 +1,13 @@
+//
+//  PRSlideView.swift
+//  PRSlideView
+//
+//  Created by Elethom Hunter on 7/11/19.
+//  Copyright © 2019 Wiredcraft. All rights reserved.
+//
+
+import UIKit
+
+class PRSlideView: UIView {
+    
+}
